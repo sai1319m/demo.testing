5740d3057512278114f70633da10787972c5236f
sai123 <sai123@gmail.com>
2022-07-04 18:39:26 +0530

    done

--- calculator.java
+++ calculator.java
@@ -1,9 +1,9 @@
-public class calculator {
+public class calculator 
+{
     public int add(int a, int b)
     {
         return a + b;
     }
-<<<<<<< HEAD
 
     public int mul(int a, int b)
     {
@@ -13,11 +13,10 @@ public class calculator {
     public int div(int a, int b)
     {
         return a / b;
-=======
+    }
     public int sub(int a, int b)
     {
         return a - b;
->>>>>>> branch-2
     }
     
 }
